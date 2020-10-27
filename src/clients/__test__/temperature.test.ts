b142111557dc249926586bb19eef3a8aabc7f4af
Filipe <filipefernandes000@gmail.com>
2020-10-27 09:59:12 -0300

    Add config library and set up temperature client config

--- src/clients/__test__/temperature.test.ts
+++ src/clients/__test__/temperature.test.ts
@@ -1,9 +1,15 @@
 import { Temperature } from "@src/clients/temperature";
+import axios from "axios";
+
+jest.mock("axios");
 
 describe("Temperature client", () => {
   it("should return normalized data from temperature service by city name ", async () => {
     const cityname = "Brasília";
-    const temperature = new Temperature();
+
+    axios.get = jest.fn().mockResolvedValue({});
+
+    const temperature = new Temperature(axios);
     const response = await temperature.fetchTemperatureByCityName(cityname);
     expect(response).toEqual({});
   });
--- src/clients/temperature.ts
+++ src/clients/temperature.ts
@@ -1,5 +1,20 @@
+import { AxiosStatic } from "axios";
+import config, { IConfig } from "config";
+
+const temperatureResourceConfig: IConfig = config.get(
+  "App.resources.Temperature"
+);
+
 export class Temperature {
+  readonly temperatureUnit = "metric";
+
+  constructor(protected request: AxiosStatic) {}
+
   public async fetchTemperatureByCityName(cityName: string): Promise<{}> {
-    return Promise.resolve({});
+    return this.request.get(
+      `${temperatureResourceConfig.get("apiUrl")}?q=${cityName}&units=${
+        this.temperatureUnit
+      }&appid=${temperatureResourceConfig.get("apiToken")}`
+    );
   }
 }
